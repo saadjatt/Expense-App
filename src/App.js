e7b6e72107d7024fb87439a002fdb65212a16507
saadcoder <msaad@vfairs.com>
2021-09-25 20:25:32 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses';
 
 function App() {
+
+  const expenses=[
+    {
+      'id':1,
+      'title':'Tea',
+      'amount':10,
+      'date':new Date(2021, 7,12)
+    },
+    {
+      'id':2,
+      'title':'Sugar',
+      'amount':100,
+      'date':new Date(2021, 7,12)
+    },
+    {
+      'id':3,
+      'title':'Milk',
+      'amount':150,
+      'date':new Date(2021, 7,12)
+    }
+  ]
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Expenses item={expenses}></Expenses>
   );
 }
 
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import './ExpenseDate.css'
+function ExpenseDate(props){
+    const day= props.date.toLocaleString('en-US',{'day':'2-digit'});
+    const month= props.date.toLocaleString('en-US',{'month':'long'});
+    const year= props.date.getFullYear();
+  
+    return (
+    <div className="expense-date">
+        <div className="expense-date__day" >{day}</div>
+        <div className="expense-month__day">{month}</div>
+        <div className="expense-year__day">{year}</div>
+    </div>
+    );
+}
+export default ExpenseDate
